b2bd5cbb07519614bdc4d37b05ef91afbefc14dd
Stalin Acurio <sta_vla411@hotmail.com>
2022-12-29 13:08:25 -0500

    bugs

--- src/services/personServer.js
+++ src/services/personServer.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://kind-red-turtle-tie.cyclic.app/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
